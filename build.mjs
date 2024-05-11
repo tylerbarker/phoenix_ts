f0ed9c406d5895b8c5437c0a5f76804bbb952f70
Tyler Barker <tyler@prototape.fm>
2024-05-11 22:40:38 +1000

    fail builds loudly please

--- build.mjs
+++ build.mjs
@@ -1,8 +1,13 @@
 import Bun from "bun";
 
-await Bun.build({
+const result = await Bun.build({
   entrypoints: ["./phoenix/index.ts"],
   outdir: "./dist",
   minify: true,
   target: "browser",
 });
+
+if (!result.success) {
+  console.error(result);
+  throw Error("Encountered an error during build.");
+}
